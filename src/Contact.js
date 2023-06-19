43292e37e25ac237f4154c898ec09dce73456f87
Bryce Capuano <bryce.capuano@gmail.com>
2023-06-19 14:32:17 -0400

    ahh

---
+++ src/Contact.js
@@ -0,0 +1,21 @@
+import "./App.css";
+
+function Contact() {
+  return (
+    
+    <div>
+            <p className="subheader">Contact Me</p>
+            <p className="subheaderContent">
+            <a class="contactB" href="https://www.linkedin.com/in/bryce-capuano/" target="_blank" rel="noreferrer">Linkedin</a>
+            </p>
+            <p className="subheaderContent">
+            bryce.capuano@gmail.com
+            </p>
+            <p className="subheaderContent">
+            650-391-3021
+            </p>
+    </div>    
+  );
+}
+
+export default Contact;
---
+++ src/Resume.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import { Document, Page } from 'react-pdf';
+
+function Resume() {
+    return (
+        <div>
+            <p className="subheader">Resume</p>
+            <p className="subheaderContent">
+                My Whole Resume
+            </p>
+        </div>    
+        );
+        
+}
+
+export default Resume;
