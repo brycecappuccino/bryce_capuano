075d811e8501d8c3edad371b1aef182d73a6675a
Bryce Capuano <bryce.capuano@gmail.com>
2023-06-23 14:49:01 -0400

    bullets fix 2 electric boogaloo

--- src/Projects.js
+++ src/Projects.js
@@ -69,7 +69,7 @@ function Projects() {
         <li> HTML/Javascript/CSS </li>
         <li> Github Pages </li>
       </ul>
-      <div>
+      </div>
       <p className="subheader">Navy SEAL/Cookie Eating/Pizza Eating Game (2013)</p>
       <p className="subheaderContent">
         <a href="https://www.khanacademy.org/computer-programming/navy-sealcookie-eating-game/2370549969" target="_blank" rel="noreferrer">
